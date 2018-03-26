6ed9b1408696157b4a8b0a448c71b6ac2c4b812d
Mai amer <may.abdelhady4405@el-eng.menofia.edu.eg>
2018-03-26 02:10:22 +0200

    modified in activities added some java code for main activity and discover activity

--- app/src/main/java/com/example/mayouza/activities/main_assessments.java
+++ app/src/main/java/com/example/mayouza/activities/main_assessments.java
@@ -1,7 +1,11 @@
 package com.example.mayouza.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.view.View;
+import android.widget.TextView;
 
 public class main_assessments extends Activity {
 
@@ -9,5 +13,42 @@ public class main_assessments extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_assessments);
+
+
+        final TextView doneTxt = (TextView) findViewById(R.id.done_assessments);
+        TextView discoverTxt = (TextView) findViewById(R.id.discover_assessments);
+        TextView overAllTxt = (TextView) findViewById(R.id.overAll_Report);
+        TextView howToTxt = (TextView) findViewById(R.id.howToDo_assessment);
+
+        doneTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), done_assessments.class);
+                startActivity(intent);
+            }
+        });
+
+        discoverTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), discoverAssessment.class);
+                startActivity(intent);
+            }
+        });
+
+        overAllTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), done_assessments.class);
+                startActivity(intent);
+            }
+        });
+        howToTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), how_to.class);
+                startActivity(intent);
+            }
+        });
     }
 }
