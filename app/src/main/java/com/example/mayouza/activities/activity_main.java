f7971537c97c8934f11f2ba2af9c66e2a9d899c7
Mai amer <may.abdelhady4405@el-eng.menofia.edu.eg>
2018-07-02 22:27:01 +0200

    style main screen and lessons activity

--- app/src/main/java/com/example/mayouza/activities/activity_main.java
+++ app/src/main/java/com/example/mayouza/activities/activity_main.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class activity_main extends AppCompatActivity {
@@ -13,12 +14,28 @@ public class activity_main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activity_main);
         TextView discoverSkills = findViewById(R.id.main_discover_skills);
         TextView currentSkills = findViewById(R.id.main_current_skills);
         TextView assessments = findViewById(R.id.main_assessments);
         TextView about = findViewById(R.id.main_about_app);
+        ImageView discover = findViewById(R.id.mainDiscoverSkillsImg);
+        ImageView current = findViewById(R.id.mainCurrentSkillsImg);
+        ImageView assessment = findViewById(R.id.mainAssessmentsImg);
+        ImageView aboutus = findViewById(R.id.mainAboutAppImg);
 
-        discoverSkills.setOnClickListener(new View.OnClickListener() {
+        discoverSkills.setText("Discover Skills");
+        currentSkills.setText("Current Skills");
+        assessments.setText("Assessments");
+        about.setText("About Us");
+
+        discover.setImageResource(R.drawable.discoverskills);
+        current.setImageResource(R.drawable.currentskills);
+        assessment.setImageResource(R.drawable.mainassessments);
+        aboutus.setImageResource(R.drawable.aboutus);
+
+
+        discover.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), activity_discover_skills.class);
@@ -26,7 +43,7 @@ public class activity_main extends AppCompatActivity {
             }
         });
 
-        currentSkills.setOnClickListener(new View.OnClickListener() {
+        current.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), current_skills.class);
@@ -34,14 +51,14 @@ public class activity_main extends AppCompatActivity {
             }
         });
 
-        assessments.setOnClickListener(new View.OnClickListener() {
+        assessment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), main_assessments.class);
                 startActivity(intent);
             }
         });
-        about.setOnClickListener(new View.OnClickListener() {
+        aboutus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), activity_about_app.class);
